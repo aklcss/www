d6bb50d15bdc300558db64014162bcf95da11616
Nathan Stewart-Devereux <n.stewart.devereux@gmail.com>
2019-06-01 21:03:49 +1200

    Swap to the gorgeous Fira Sans

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,14 +1,31 @@
 import Typography from "typography";
 import lincolnTheme from "typography-theme-lincoln";
 
-lincolnTheme.baseFontSize = "15px";
+lincolnTheme.baseFontSize = "19px";
 lincolnTheme.baseLineHeight = "1.58";
 lincolnTheme.scaleRatio = "1.6";
-lincolnTheme.overrideThemeStyles = ({ rhythm }, options, styles) => ({
-  "a": {
-    textShadow: 0
-  }
-});
+lincolnTheme.headerFontFamily = [
+  "Fira Sans",
+  "sans-serif"
+];
+lincolnTheme.bodyFontFamily = [
+  "Fira Sans",
+  "sans-serif"
+];
+lincolnTheme.googleFonts = [
+  {
+    name: "Fira Sans",
+    styles: [
+      "400",
+      "900"
+    ]
+  },
+];
+  lincolnTheme.overrideThemeStyles = ({ rhythm }, options, styles) => ({
+    "a": {
+      textShadow: 0
+    }
+  });
 const typography = new Typography(lincolnTheme);
 
 export default typography;
